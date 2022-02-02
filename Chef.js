a2de271c38e8fe235bde5aeda32132810020c13b
Caleb Trachte <cetrachte@hotmail.com>
2022-02-02 13:04:11 -0600

    fixed regex bugs, add index.html entry point, add example instance of Chef, added values to markdownScore

--- Chef.js
+++ Chef.js
@@ -2,7 +2,7 @@
 
 // need a class that takes in a string, markdown or html and then maps to a JSON object
 
-class TheChef {
+export default class TheChef {
     constructor (props) {
         this.raw = props.raw,
         this.json = props.json,
@@ -10,7 +10,8 @@ class TheChef {
         this.html = props.html,
         this.markdown = props.markdown,
         this.markdownScore = this.markdownScore.bind(this),
-        this.WhatTypeIsThe = this.WhatTypeIsThe.bind(this)
+        this.WhatTypeIsThe = this.WhatTypeIsThe.bind(this),
+        this.markdownScore(this.raw)
     }
     //parent method to determine type
     WhatTypeIsThe(raw) {
@@ -24,16 +25,16 @@ class TheChef {
     }
     // creates a scoring system to weight the chances that the string is a markdown string
     markdownScore(raw) {
-        this.RegExpScore = new Object();
-        RegExpScore.matchHeading1 = /^#{2}[^#].*/gm.test(raw); // very low score
-        RegExpScore.matchHeading2 = /^#{2}[^#].*/gm.test(raw); // low score
-        RegExpScore.matchHeading3 = /^#{3}[^#].*/gm.test(raw); // medium score
-        RegExpScore.matchHeading4 = /^#{4}[^#].*/gm.test(raw); // medium score
-        RegExpScore.matchHeading5 = /^#{5}[^#].*/gm.test(raw); // high score
-        RegExpScore.matchHeading6 = /^#{6}[^#].*/gm.test(raw); // very high score
-        RegExpScore.matchBold = /^(*{2}|_{2}){1}(.+)+\1$/gm.test(raw); // medium score
-        RegExpScore.matchItalic = /\*.*\*/gm.test(raw); // low score
-        RegExpScore.matchLink = /\[(.+)\]\(([^ ]+?)( "(.+)")?\)/gm.test(raw); // high score
-        console.log(this.RegExpScore)
+        let RegExpScore = new Object();
+        RegExpScore.matchHeading1 = /^#{2}[^#].*/gm.test(raw) ? 5 : 0; // very low score
+        RegExpScore.matchHeading2 = /^#{2}[^#].*/gm.test(raw) ? 10 : 0; // low score
+        RegExpScore.matchHeading3 = /^#{3}[^#].*/gm.test(raw) ? 15 : 0; // medium score
+        RegExpScore.matchHeading4 = /^#{4}[^#].*/gm.test(raw) ? 15 : 0; // medium score
+        RegExpScore.matchHeading5 = /^#{5}[^#].*/gm.test(raw) ? 20 : 0; // high score
+        RegExpScore.matchHeading6 = /^#{6}[^#].*/gm.test(raw) ? 30 : 0; // very high score
+        // RegExpScore.matchBold = /^(*{2}|_{2}){1}(.+)+\1$/gm.test(raw); // medium score
+        RegExpScore.matchItalic = /\*.*\*/gm.test(raw) ? 5 : 0; // low score
+        RegExpScore.matchLink = /\[(.+)\]\(([^ ]+?)( "(.+)")?\)/gm.test(raw) ? 50 : 0; // very high score
+        console.log(RegExpScore)
     }
 }
