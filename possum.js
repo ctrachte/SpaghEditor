bf6d26fcc9e6e18673ab22798dac06ec10f374ef
Caleb Trachte <cetrachte@hotmail.com>
2022-03-09 15:01:27 -0600

    add web crypto api for additional crypto methods

--- possum.js
+++ possum.js
@@ -8,18 +8,20 @@ export default class Possum {
     this.destroy = this.destroy.bind(this);
     this.janitor = this.janitor.bind(this);
     this.takeBait = this.takeBait.bind(this);
-    // Possum destroys the spagetti
-    this.mess = this.destroy();
-    // janitor cleans up the mess
-    this.clean = this.janitor();
-    console.log(
-      "original spaghetti (string):",
-      spaghetti,
-      "after possum makes a mess:",
-      this.mess,
-      "after Janitor Cleans:",
-      this.clean
-    );
+    this.generateKey = this.generateKey.bind(this);
+    // // Possum destroys the spagetti
+    // this.mess = this.destroy();
+    // // janitor cleans up the mess
+    // this.clean = this.janitor();
+    // console.log(
+    //   "original spaghetti (string):",
+    //   spaghetti,
+    //   "after possum makes a mess:",
+    //   this.mess,
+    //   "after Janitor Cleans:",
+    //   this.clean
+    // );
+    this.generateKey(this.spaghetti.toString());
   }
   destroy = () =>
     this.spaghetti
@@ -35,17 +37,13 @@ export default class Possum {
       .map(this.takeBait)
       .map(this.reorganize)
       .join("");
-  makeMess = (spaghetti) =>
-    spaghetti.split("").map(this.codepoint);
+  makeMess = (spaghetti) => spaghetti.split("").map(this.codepoint);
   takeByte = (noodle) => {
     // console.log(Number(noodle))
     let number = "0" + Number(noodle).toString(16);
     // console.log(number.length);
     if (number.length !== 2) {
-      return number.substring(
-        (number.length - 2),
-        (number.length)
-      )
+      return number.substring(number.length - 2, number.length);
     } else {
       return number;
     }
@@ -59,8 +57,66 @@ export default class Possum {
     // console.log(mess);
     return String.fromCodePoint("0" + mess);
   };
-  codepoint (string) {
-      // console.log(string, string.codePointAt(0))
-      return string.codePointAt(0);
-  }
+  codepoint(string) {
+    // console.log(string, string.codePointAt(0))
+    return string.codePointAt(0);
+  };
+  stringToArrayBuffer(str) {
+    const buff = new ArrayBuffer(str.length * 2); // Because there are 2 bytes for each char.
+    const buffView = new Uint16Array(buff);
+    for (let i = 0, strLen = str.length; i < strLen; i++) {
+      buffView[i] = str.codePointAt(i);
+    }
+    return buff;
+  };
+  arrayBufferToString(buff) {
+    return String.fromCodePoint.apply(null, new Uint16Array(buff));
+  };
+  async generateKey(string) {
+    let context = this;
+    const stringToEncrypt = string || context.spaghetti;
+    console.log(stringToEncrypt)
+    // https://github.com/diafygi/webcrypto-examples#rsa-oaep---generatekey
+    // The resultant publicKey will be used to encrypt
+    // and the privateKey will be used to decrypt.
+    // Note: This will generate new keys each time, you must store both of them in order for
+    // you to keep encrypting and decrypting.
+    //
+    // I warn you that storing them in the localStorage may be a bad idea, and it gets out of the scope
+    // of this post.
+    context.key = await crypto.subtle.generateKey(
+      {
+        name: "RSA-OAEP",
+        modulusLength: 4096,
+        publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
+        hash: { name: "SHA-512" },
+      },
+      true,
+      // This depends a lot on the algorithm used
+      // Look down below https://auth0.com/docs/protocols/oauth2/redirect-users
+      // and see the table. Since we're using RSA-OAEP we have encrypt and decrypt
+      // available
+      ["encrypt", "decrypt"]
+    );
+    console.log(context.key)
+
+    // key will yield a key.publicKey and key.privateKey property.
+
+    context.encryptedUri = await crypto.subtle.encrypt(
+      {
+        name: "RSA-OAEP",
+      },
+      context.key.publicKey,
+      context.stringToArrayBuffer(stringToEncrypt)
+    );
+    console.log("encrypted:", context.arrayBufferToString(context.encryptedUri))
+    context.msg = await crypto.subtle.decrypt(
+      {
+        name: "RSA-OAEP",
+      },
+      context.key.privateKey,
+      context.encryptedUri
+    );
+    console.log('decrypted', context.arrayBufferToString(context.msg))
+  };
 }
