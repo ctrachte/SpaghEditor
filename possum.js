21ed8eb70a814a326f120630500fb0449e0bc763
Caleb Trachte <cetrachte@hotmail.com>
2022-03-09 11:54:15 -0600

    more renaming, more conversion to lambda

--- possum.js
+++ possum.js
@@ -1,35 +1,45 @@
 export default class Possum {
-    // TODO: trap should be like a hashing key? implement hashing
+  // TODO: trap should be like a hashing key? implement hashing
   constructor(spaghetti, bait, trap) {
     this.spaghetti = spaghetti;
     this.bait = bait; // salt
     this.trap = trap; // hash key
     this.destroy = this.destroy.bind(this);
-    this.cleanUp = this.cleanUp.bind(this);
+    this.janitor = this.janitor.bind(this);
     this.takeBait = this.takeBait.bind(this);
     // Possum destroys the spagetti
-    this.mess = this.destroy(); // -> 426f666665
-    // chef cleans up the mess
-    this.clean = this.cleanUp(); // -> Hello
-    console.log("original input:", spaghetti, "mess:", this.mess, "cleanUp:", this.clean);
+    this.mess = this.destroy();
+    // janitor cleans up the mess
+    this.clean = this.janitor();
+    console.log(
+      "original input:",
+      spaghetti,
+      "mess:",
+      this.mess,
+      "Janitor:",
+      this.clean
+    );
   }
-  makeMess = (spaghetti) => spaghetti.split("").map((noodle) => noodle.charCodeAt(0))
-  takeByte = (noodle) => ("0" + Number(noodle).toString(16)).substring(("0" + Number(noodle).toString(16)).length-2,("0" + Number(noodle).toString(16)).length)
+  makeMess = (spaghetti) =>
+    spaghetti.split("").map((noodle) => noodle.charCodeAt(0));
+  takeByte = (noodle) =>
+    ("0" + Number(noodle).toString(16)).substring(
+      ("0" + Number(noodle).toString(16)).length - 2,
+      ("0" + Number(noodle).toString(16)).length
+    );
   takeBait = (mess) => this.makeMess(this.bait).reduce((a, b) => a ^ b, mess);
-  destroy() {
-    return this.spaghetti
+  destroy = () =>
+    this.spaghetti
       .split("")
       .map(this.makeMess)
       .map(this.takeBait)
       .map(this.takeByte)
       .join("");
-  }
-  cleanUp() {
-    return this.mess
+  janitor = () =>
+    this.mess
       .match(/.{1,2}/g)
-      .map((hex) => parseInt(hex, 16))
+      .map((scoop) => parseInt(scoop, 16))
       .map(this.takeBait)
-      .map((charCode) => String.fromCharCode(charCode))
+      .map((noodle) => String.fromCharCode(noodle))
       .join("");
-  }
 }
