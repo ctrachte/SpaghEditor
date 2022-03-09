c17a24bf9424e315a5205de10e11729bf64e439e
Caleb Trachte <cetrachte@hotmail.com>
2022-03-09 10:33:05 -0600

    add basic crypto example

---
+++ spaghetify.js
@@ -0,0 +1,63 @@
+const cipher = salt => {
+    const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+    const byteHex = n => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+
+    return text => text.split('')
+      .map(textToChars)
+      .map(applySaltToChar)
+      .map(byteHex)
+      .join('');
+}
+    
+const decipher = salt => {
+    const textToChars = text => text.split('').map(c => c.charCodeAt(0));
+    const applySaltToChar = code => textToChars(salt).reduce((a,b) => a ^ b, code);
+    return encoded => encoded.match(/.{1,2}/g)
+      .map(hex => parseInt(hex, 16))
+      .map(applySaltToChar)
+      .map(charCode => String.fromCharCode(charCode))
+      .join('');
+}
+
+// To create a cipher
+const myCipher = cipher('mySecretSalt')
+
+//Then cipher any text:
+console.log(myCipher('the secret string'))
+
+//To decipher, you need to create a decipher and use it:
+const myDecipher = decipher('mySecretSalt')
+console.log(myDecipher("7c606d287b6d6b7a6d7c287b7c7a61666f"))
+
+const crypt = (salt, text) => {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+  
+    return text
+      .split("")
+      .map(textToChars)
+      .map(applySaltToChar)
+      .map(byteHex)
+      .join("");
+  };
+  
+  const decrypt = (salt, encoded) => {
+    const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+    const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+    return encoded
+      .match(/.{1,2}/g)
+      .map((hex) => parseInt(hex, 16))
+      .map(applySaltToChar)
+      .map((charCode) => String.fromCharCode(charCode))
+      .join("");
+  };
+
+  // encrypting
+const encrypted_text = crypt("salt", "Hello"); // -> 426f666665
+
+// decrypting
+const decrypted_string = decrypt("salt", "426f666665"); // -> Hello
+
+console.log('encrypt:', encrypted_text, 'decrypt', decrypted_string)
