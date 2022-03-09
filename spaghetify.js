76df9ebef60400bfd1d0016302d216ba41d3417b
Caleb Trachte <cetrachte@hotmail.com>
2022-03-09 11:50:15 -0600

    replaced deprecated String.prototype.substr with newer method, changed syntax to lambda methods, fixed bad refs

--- spaghetify.js
+++ spaghetify.js
@@ -6,21 +6,16 @@ export default class Possum {
     this.trap = trap; // hash key
     this.destroy = this.destroy.bind(this);
     this.cleanUp = this.cleanUp.bind(this);
+    this.takeBait = this.takeBait.bind(this);
     // Possum destroys the spagetti
-    this.mess = this.destroy(this.bait, this.spaghetti); // -> 426f666665
+    this.mess = this.destroy(); // -> 426f666665
     // chef cleans up the mess
-    this.clean = this.cleanUp(this.bait, this.mess); // -> Hello
-    console.log("mess:", this.mess, "cleanUp", this.clean);
+    this.clean = this.cleanUp(); // -> Hello
+    console.log("original input:", spaghetti, "mess:", this.mess, "cleanUp:", this.clean);
   }
-  makeMess(spaghetti) {
-    return spaghetti.split("").map((noodle) => noodle.charCodeAt(0));
-  }
-  takeByte(noodle) {
-    return ("0" + Number(noodle).toString(16)).substring(-2);
-  }
-  takeBait(bait) {
-    return this.makeMess(bait).reduce((a, b) => a ^ b, bait);
-  }
+  makeMess = (spaghetti) => spaghetti.split("").map((noodle) => noodle.charCodeAt(0))
+  takeByte = (noodle) => ("0" + Number(noodle).toString(16)).substring(("0" + Number(noodle).toString(16)).length-2,("0" + Number(noodle).toString(16)).length)
+  takeBait = (mess) => this.makeMess(this.bait).reduce((a, b) => a ^ b, mess);
   destroy() {
     return this.spaghetti
       .split("")
@@ -30,7 +25,7 @@ export default class Possum {
       .join("");
   }
   cleanUp() {
-    return this.spaghetti
+    return this.mess
       .match(/.{1,2}/g)
       .map((hex) => parseInt(hex, 16))
       .map(this.takeBait)
