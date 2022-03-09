0e00620528fda38ad074db67386b445dc068994d
Caleb Trachte <cetrachte@hotmail.com>
2022-03-09 11:21:01 -0600

    fixed context issue in constructor, refs

--- spaghetify.js
+++ spaghetify.js
@@ -5,20 +5,20 @@ export default class Possum {
     this.trap = trap; // hash key
     this.destroy = this.destroy.bind(this);
     this.cleanUp = this.cleanUp.bind(this);
-    this.makeMess = this.makeMess.bind(this);
-    this.takeBait = this.takeBait.bind(this);
     // Possum destroys the spagetti
-    this.mess = destroy("salt", "Hello"); // -> 426f666665
+    this.mess = this.destroy(this.bait, this.spaghetti); // -> 426f666665
     // chef cleans up the mess
-    this.cleanUp = cleanUp("salt", "426f666665"); // -> Hello
-    console.log("mess:", this.mess, "cleanUp", this.cleanUp);
+    this.clean = this.cleanUp(this.bait, this.mess); // -> Hello
+    console.log("mess:", this.mess, "cleanUp", this.clean);
   }
   makeMess(spaghetti) {
-    spaghetti.split("").map((noodle) => noodle.charCodeAt(0));
+    return spaghetti.split("").map((noodle) => noodle.charCodeAt(0));
   }
-  takeByte = (noodle) => ("0" + Number(noodle).toString(16)).substring(-2);
+  takeByte(noodle) {
+    return ("0" + Number(noodle).toString(16)).substring(-2);
+  }
   takeBait(bait) {
-    this.makeMess(bait).reduce((a, b) => a ^ b, bait);
+    return this.makeMess(bait).reduce((a, b) => a ^ b, bait);
   }
   destroy() {
     return this.spaghetti
