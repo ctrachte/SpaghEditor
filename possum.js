1ed84f03ff2b34b284271a89737f92b8dc393ca4
Caleb Trachte <cetrachte@hotmail.com>
2022-03-09 14:04:55 -0600

    minor updates to example, testing ascii values

--- possum.js
+++ possum.js
@@ -12,14 +12,14 @@ export default class Possum {
     this.mess = this.destroy();
     // janitor cleans up the mess
     this.clean = this.janitor();
-    // console.log(
-    //   "original input:",
-    //   spaghetti,
-    //   "mess:",
-    //   this.mess,
-    //   "Janitor:",
-    //   this.clean
-    // );
+    console.log(
+      "original spaghetti (string):",
+      spaghetti,
+      "after possum makes a mess:",
+      this.mess,
+      "after Janitor Cleans:",
+      this.clean
+    );
   }
   destroy = () =>
     this.spaghetti
@@ -36,14 +36,19 @@ export default class Possum {
       .map(this.reorganize)
       .join("");
   makeMess = (spaghetti) =>
-    spaghetti.split("").map((noodle) => noodle.codePointAt(0));
+    spaghetti.split("").map(this.codepoint);
   takeByte = (noodle) => {
+    // console.log(Number(noodle))
     let number = "0" + Number(noodle).toString(16);
-    // console.log(number);
-    return number.substring(
-      ("0" + Number(noodle).toString(16)).length - 2,
-      ("0" + Number(noodle).toString(16)).length
-    );
+    // console.log(number.length);
+    if (number.length !== 2) {
+      return number.substring(
+        (number.length - 2),
+        (number.length)
+      )
+    } else {
+      return number;
+    }
   };
   takeBait = (mess) => this.makeMess(this.bait).reduce((a, b) => a ^ b, mess);
   mop = (pile) => {
@@ -54,4 +59,8 @@ export default class Possum {
     // console.log(mess);
     return String.fromCodePoint("0" + mess);
   };
+  codepoint (string) {
+      // console.log(string, string.codePointAt(0))
+      return string.codePointAt(0);
+  }
 }
